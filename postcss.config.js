6001fb2a4514228adcbc3416042033f9c5e0935d
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-01-24 10:41:09 +0100

    lint:fix

--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,13 @@
-const purgecssConfig = {
-  content: ['./components/**/*.js', './pages/**/*.js'],
-  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
-}
+// const purgecssConfig = {
+//   content: ['./components/**/*.js', './pages/**/*.js'],
+//   defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+// }
 
 module.exports = {
   plugins: {
     'postcss-import': {},
     tailwindcss: {},
-    autoprefixer: {},
-    //'@fullhuman/postcss-purgecss': purgecssConfig
+    autoprefixer: {}
+    // '@fullhuman/postcss-purgecss': purgecssConfig
   }
 }
