837bccf0cdac0c95012150a964053f97fc736c99
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-01-27 08:38:03 +0100

    Oppdaterer småfikser for css (patch)

---
+++ components/dummy-styles.js
@@ -0,0 +1,16 @@
+const Dummy = props => {
+  return (
+    <>
+      <h1>H1</h1>
+      <h2>H2</h2>
+      <h3>H3</h3>
+      <ul>
+        <li>li</li>
+      </ul>
+      <b>b</b>
+      <img src='images/mega-happy-face.png' alt='image' />
+    </>
+  )
+}
+
+export default Dummy
